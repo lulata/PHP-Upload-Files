01f4f4ec3d746c3aa7958da806365579f0228f1a
David Atanasoski <davidatanasoski@hotmail.com>
2022-09-18 11:37:28 +0200

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="uploads.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="file" >
+      <button type="submit" name="submit"> Upload</button>
+    </form>
+  </body>
+</html>
